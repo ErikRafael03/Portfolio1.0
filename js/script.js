cc69b455426f91246e81f0264bf53616fd9cd010
Erik Rafael Alves Santos <erikrafaelasantos@gmail.com>
2024-05-12 01:41:05 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// inicio MENU MOBILE
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('nav-mobile')
 let overlay = document.getElementById('overlay-menu')
@@ -12,4 +13,25 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+// fim MENU MOBILE
 
+// INICIO ANIMAÇÃO SCROLL
+const elements = document.querySelectorAll('.hidden')
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        // else{
+        //     entry.target.classList.remove('show')
+        // }
+    })
+})
+
+elements.forEach((element) => myObserver.observe(element))
+
+
+// efeito scroll inserido com sucesso apenas na section 2. Rever o tempo de animação!!!!
+
+// FIM ANIMAÇÃO SCROLL
