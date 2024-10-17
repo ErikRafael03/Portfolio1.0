aa4c0f6515217aa20c1267c3d8ea1523b1e2e84f
Erik Rafael Alves Santos <erikrafaelasantos@gmail.com>
2024-10-16 21:42:20 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,22 +1,134 @@
-// MENU MOBILE 
+// inicio MENU MOBILE
 let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
+let menu = document.getElementById('nav-mobile')
+let overlay = document.getElementById('overlay-menu')
 
 btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu')
 })
+
 menu.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
-// MENU MOBILE 
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+// fim MENU MOBILE
 
-// MENU DESKTOP
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header'); // Seleciona o elemento header
-    if (window.scrollY > 50) { // Altere o valor 50px conforme necessário
-        header.classList.add('scrolled'); // Adiciona a classe 'scrolled' após o scroll
-    } else {
-        header.classList.remove('scrolled'); // Remove a classe quando o scroll for menor
-    }
-});
-// MENU DESKTOP
+// INICIO ANIMAÇÃO SCROLL
+const elements = document.querySelectorAll('.hidden')
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+        // else{
+        //     entry.target.classList.remove('show')
+        // }
+    })
+})
+
+elements.forEach((element) => myObserver.observe(element))
+
+
+// efeito scroll inserido com sucesso apenas na section 2. Rever o tempo de animação!!!!
+
+// FIM ANIMAÇÃO SCROLL
+
+function mostrartxt1() {
+    var txt = document.getElementById('txt1')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'none'
+    txtF.style.display = 'block'
+}
+function tirartxt1() {
+    var txt = document.getElementById('txt1')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'block'
+    txtF.style.display = 'none'
+}
+function mostrartxt2() {
+    var txt = document.getElementById('txt2')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'none'
+    txtF.style.display = 'block'
+}
+function tirartxt2() {
+    var txt = document.getElementById('txt2')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'block'
+    txtF.style.display = 'none'
+}
+function mostrartxt3() {
+    var txt = document.getElementById('txt3')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'none'
+    txtF.style.display = 'block'
+}
+function tirartxt3() {
+    var txt = document.getElementById('txt3')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'block'
+    txtF.style.display = 'none'
+}
+function mostrartxt4() {
+    var txt = document.getElementById('txt4')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'none'
+    txtF.style.display = 'block'
+}
+function tirartxt4() {
+    var txt = document.getElementById('txt4')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'block'
+    txtF.style.display = 'none'
+}
+function mostrartxt5() {
+    var txt = document.getElementById('txt5')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'none'
+    txtF.style.display = 'block'
+}
+function tirartxt5() {
+    var txt = document.getElementById('txt5')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'block'
+    txtF.style.display = 'none'
+}
+function mostrartxt6() {
+    var txt = document.getElementById('txt6')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'none'
+    txtF.style.display = 'block'
+}
+function tirartxt6() {
+    var txt = document.getElementById('txt6')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'block'
+    txtF.style.display = 'none'
+}
+function mostrartxt7() {
+    var txt = document.getElementById('txt7')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'none'
+    txtF.style.display = 'block'
+}
+function tirartxt7() {
+    var txt = document.getElementById('txt7')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'block'
+    txtF.style.display = 'none'
+}
+function mostrartxt8() {
+    var txt = document.getElementById('txt8')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'none'
+    txtF.style.display = 'block'
+}
+function tirartxt8() {
+    var txt = document.getElementById('txt8')
+    var txtF = document.getElementById('txtfixo')
+    txt.style.display = 'block'
+    txtF.style.display = 'none'
+}
